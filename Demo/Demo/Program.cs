23f02aec836c21dcd26b36062da09534099a3939
AZALEA\SabanaTech <rescalante@sabanatech.com>
2023-08-04 15:01:26 -0600

    FEATURE: Se cambia el mensaje del saludo

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("¡Hola mundo!");
-Console.WriteLine("Esta es una prueba.");
+Console.WriteLine("Esta es una prueba en el nuevo branch.");
